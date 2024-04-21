dc50229dee4cf8ebf42b3e38a15bf8a4836df924
Ermaldo09 <ermal.bajraktari06@gmail.com>
2024-04-21 12:48:52 +0200

    Search view Created and the required routes are set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from "./views/Search";
 
 function App() {
 
   return (
-      <div className="App">
-          App
-      </div>
+      <Router>
+          <div className="App">
+              <Routes>
+                  <Route path="/" element={<Search />} />
+              </Routes>
+          </div>
+      </Router>
   )
 }
 
